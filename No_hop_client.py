50e0be9ac040a2a51d3c7ab5e6d9aef952976a1c
Lily <lilyhuegerich@Lill.fritz.box>
2021-09-30 19:44:39 +0200

    fix range bug

--- No_hop_client.py
+++ No_hop_client.py
@@ -210,5 +210,5 @@ def send_No_hop(ip="10.0.1.1", ID=0, message="DHT message for testing" ,message_
 
 if __name__ == "__main__":
 
-    host= No_hop_host(client=True)
+    host= No_hop_host(client=False)
     host.run()
