fc9a5e4fa7b51aa248b540b495279ea9f06bdda8
Lily <lilyhuegerich@Lill.fritz.box>
2021-10-08 17:53:09 +0200

    new utisl

--- No-hop_controller.py
+++ No-hop_controller.py
@@ -8,11 +8,14 @@ from time import sleep
 sys.path.append(
     os.path.join(os.path.dirname(os.path.abspath(__file__)),
                  './utils/'))
+
+SWITCH_TO_HOST_PORT = 1
+SWITCH_TO_SWITCH_PORT = 2
+
 import p4runtime_lib.bmv2
 #from p4runtime_lib.error_utils import printGrpcError
 from p4runtime_lib.switch import ShutdownAllSwitchConnections
 import p4runtime_lib.helper
-
 
 
 def controller():
