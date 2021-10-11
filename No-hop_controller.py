b3c8df7db770a3e79e491e5ffb12d5d81aa08d04
Lily <lilyhuegerich@Lill.fritz.box>
2021-10-11 17:11:32 +0200

    controller tests

--- No-hop_controller.py
+++ No-hop_controller.py
@@ -49,7 +49,7 @@ def controller():
         s_l.append(p4runtime_lib.bmv2.Bmv2SwitchConnection(
                 name='s'+str(i),
                 address='127.0.0.1:5005'+str(i),
-                device_id=0))
+                device_id=i-1))
         i+=1
         s_l[-1].MasterArbitrationUpdate(role=3, election_id = 1)
         for entry in s_l[-1].ReadTableEntries():
