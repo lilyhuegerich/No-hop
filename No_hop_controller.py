6dc8a3a7becdd4fac9dc8003f22e1d3720cff235
Lily <lilyhuegerich@Lill.fritz.box>
2021-10-13 19:45:48 +0200

    run ex

--- No_hop_controller.py
+++ No_hop_controller.py
@@ -285,7 +285,7 @@ class controller():
             for e in entry.entities:
                 #print e.table_entry
                 #print dir(e.table_entry)
-                print (dir(e.table_entry.action.action.params))
+                print (dir(e.table_entry.action.action.params._values))
                 print (e.table_entry.table_id, e.table_entry.table_id, str(e.table_entry.action["action"]["params"]["value"]).split("\0")[-1])
                 print (str(new_entry.action["action_params"][port]))
                 if (e.table_entry.table_id== self.no_hop_table_id and str(new_entry.action["action_params"][port]) in str(e.table_entry.action["action"]["params"]["value"]).split("\0")[-1]):
