e466c593992ecfff0ad92ab93fd1209b5d4bb374
Lily <lilyhuegerich@Lill.fritz.box>
2021-10-13 20:23:35 +0200

    run ex

--- No_hop_controller.py
+++ No_hop_controller.py
@@ -296,7 +296,7 @@ class controller():
             priority=priority)
         try:
             #to_change.s.WriteTableEntry(table_entry)
-            #print "Added table entry"# ", table_entry
+            print "Added table entry"# ", table_entry
         except Exception as ex:
             print (ex, to_change.name)
             pass
